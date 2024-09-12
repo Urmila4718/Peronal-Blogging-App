2ed13ab8508c38770e9e49a1243b89e4845ff317
Urmila.Bhartal <Urmila.Bhartal@PT096LT>
2024-09-12 13:39:43 +0530

    Updated settings.py with static changes

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,"static_files")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATIC_URL = '/static/'
 
