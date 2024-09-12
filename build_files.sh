3eae1bf5e4e01a383a06b378cb368b1111cad1d8
Urmila.Bhartal <Urmila.Bhartal@PT096LT>
2024-09-12 13:59:34 +0530

    updated build_files.sh to ensure the pip is installed

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Ensure pip is installed
+echo "Checking for pip..."
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
